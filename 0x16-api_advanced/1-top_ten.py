404cd1d092365fa1f6b15e4f8dd3bf2636a93871
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-05-10 13:05:25 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""API Reddit"""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    """Print the titles of the first 10 hot posts
+    listed for a given subreddit"""
+
+    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url=url, headers=headers)
     if response.status_code == 404:
-        print("None")
+        print(None)
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    subs = response.json().get("data")
+    for post in subs.get("children"):
+        post_details = post.get("data")
+        print(post_details.get("title"))
