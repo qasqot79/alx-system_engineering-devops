17768d7e1f71aad78b39b16609c3313fe4ba0fcf
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-05-10 13:03:52 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
+"""API Reddit"""
 import requests
 
+
 def number_of_subscribers(subreddit):
-        """returns the number of subscribers for a given subreddit"""
-            if subreddit is None or type(subreddit) is not str:
-                        return 0
-                        r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                                                     headers={'User-Agent': '0x16-api_advanced:project:\
-                                                             v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-                                                         subs = r.get("data", {}).get("subscribers", 0)
-                                                             return subs
+    """Get number of subsribers for a given subreddit"""
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url=url, headers=headers)
+    if response.status_code != 404:
+        subs = response.json().get("data").get("subscribers")
+        return subs
+    return 0
