3482861748e5b3e2e7bd83116ef766c49ca29c22
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-05-10 14:06:19 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+#Using strace  and then using Puppet to automate fix for 500 error on Apache
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+exec { 'fix_typo':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  =>  "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  provider =>  'shell'}
