016121d64afce15b1d95134057049881d3aae3e3
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-05-11 14:59:49 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,4 @@
-# issue on line 137 of /var/www/html/wp-settings.php
-$rep = 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );'
-$settings = '/var/www/html/wp-settings.php'
-$cmd = "sed -i \"/class-wp-locale.phpp/c ${::rep}\" ${::settings}"
-exec { 'serverfix':
-  path    => '/bin',
-  command => $cmd,
-}
-# Create a manifest that fixes file name typo
+# Create a manifest that fixes why Apache is returning a 500 error
 exec { 'fix_typo':
   command => 'mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
   path    => '/bin/'
