c8b8ae284c7163a47245db279270f9fffdcb0274
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-05-11 14:57:27 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -6,3 +6,8 @@ exec { 'serverfix':
   path    => '/bin',
   command => $cmd,
 }
+# Create a manifest that fixes file name typo
+exec { 'fix_typo':
+  command => 'mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
+  path    => '/bin/'
+}
