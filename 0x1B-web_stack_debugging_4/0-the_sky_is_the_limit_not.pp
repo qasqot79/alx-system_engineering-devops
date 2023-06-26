f4d4395a74f01e216bee756b556efaaf33074f1b
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-06-26 11:43:25 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,1 @@
-# fix the limit nginx.
-exec { 'myfix':
-command => 'sed -i "/ULIMIT=/c\ULIMIT=\"-n 2000\"" /etc/default/nginx',
-path    => '/bin',
-}
-service { 'nginx':
-ensure    => running,
-subscribe => Exec['myfix'],
-}
+
