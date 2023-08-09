54da25e01955e5cb151b1cc548415bb20e063fe4
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-08-09 19:40:11 +0100

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,8 +2,10 @@
 """Function to count words in all hot posts of a given Reddit subreddit."""
 import requests
 
+
 def count_words(subreddit, word_list, instances={}, after="", count=0):
     """Prints counts of given words found in hot posts of a given subreddit.
+    
     Args:
         subreddit (str): The subreddit to search.
         word_list (list): The list of words to search for in post titles.
