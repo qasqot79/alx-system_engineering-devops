a8cdf7ba3c656ea5af2340ad63c8879118620fcc
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-08-10 14:22:05 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,6 @@
-# In wp-settings.php there is a misspell in lIne 137
-exec { 'change the error':
-  command => '/usr/bin/sudo /bin/sed -i "s/class-wp-locale.phpp/class-wp-locale.php/g" /var/www/html/wp-settings.php'
-    }
+# Fixes bad `phpp` extensions to `php`
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
