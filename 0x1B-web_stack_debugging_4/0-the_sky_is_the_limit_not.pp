010fdfaac54b351f3f3ec35058bc3d54eb65cd57
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-08-18 16:42:19 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,13 @@
-# increase number of open fd
-exec {'fd':
-path     => ['/usr/bin', '/bin'],
-command  => "sudo sed -i 's/15/2000/g' /etc/default/nginx; sudo service nginx restart",
-provider => 'shell',
+# Increases the amount of traffic an Nginx server can handle.
+
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
