158c50bc1897ac01d26abfd5edb78eafa3271a34
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-08-18 16:20:26 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,5 @@
 # Set ulimit on nginx max file descriptors to system soft limit
+
 exec {'ulimit':
   command => 'sed -r -i "s/(ULIMIT=\"-n) [0-9]+/\1 8000/" /etc/default/nginx',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
