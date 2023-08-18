ef0d3d746f1540effbb0009f681485fa8d7c810d
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-08-18 17:31:03 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 #Manifest to fix issue with LAMP stack serving Wordpress site.
+
 #Issue is a misspelled resource in `wp-settings.php`.
 exec {'sed phpp':
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
