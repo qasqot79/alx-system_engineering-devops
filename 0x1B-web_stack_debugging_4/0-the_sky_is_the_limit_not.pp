7cd39f473a4e00d4cbc86a3bb8e23dbcf08e529d
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-08-18 16:34:15 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # increase number of open fd
 exec {'fd':
 path     => ['/usr/bin', '/bin'],
-command  => "sudo sed -i 's/15/3000/g' /etc/default/nginx; sudo service nginx restart",
+command  => "sudo sed -i 's/15/2000/g' /etc/default/nginx; sudo service nginx restart",
 provider => 'shell',
 }
