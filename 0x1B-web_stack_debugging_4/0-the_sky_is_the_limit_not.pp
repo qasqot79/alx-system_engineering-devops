c52ff473e5b5c28eba86dba116595c442f87b98d
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-08-18 16:28:26 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,6 @@
-# Increases the amount of traffic an Nginx server can handle.
-
-# Set ulimit on nginx max file descriptors to system soft limit
-exec {'ulimit':
-  command => 'sed -r -i "s/(ULIMIT=\"-n) [0-9]+/\1 8000/" /etc/default/nginx',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => 'test -e /etc/default/nginx',
-}
-
-exec {'restart nginx':
-  command => 'service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  require => Exec['ulimit'],
+# increase number of open fd
+exec {'fd':
+path     => ['/usr/bin', '/bin'],
+command  => "sudo sed -i 's/15/3000/g' /etc/default/nginx; sudo service nginx restart",
+provider => 'shell',
 }
