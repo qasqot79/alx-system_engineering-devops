e7c60498ad72a89fe8a3c6f5698daefd03c7cb1f
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-11-07 11:00:08 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+Queries the Reddit API and returns the number of subscribers for a given subreddit.
+If the subreddit is invalid, it returns 0.
+"""
+
 import requests
 
 def number_of_subscribers(subreddit):
-    # Create the Reddit API URL for the given subreddit
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'MyRedditBot/1.0'}  # Set a custom User-Agent to avoid issues
 
-    # Set a custom User-Agent header to avoid rate limiting
-    headers = {'User-Agent': 'MyRedditBot/1.0'}
-
-    # Send a GET request to the Reddit API
     response = requests.get(url, headers=headers)
 
-    # Check if the response status code is 200 (OK)
     if response.status_code == 200:
-        # Parse the JSON response and extract the number of subscribers
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
     else:
-        # If the subreddit is invalid or not found, return 0
         return 0
 
 if __name__ == '__main__':
@@ -29,5 +26,5 @@ if __name__ == '__main__':
         print("Please pass an argument for the subreddit to search.")
     else:
         subreddit = sys.argv[1]
-        subscribers = number_of_subscribers(subreddit)
-        print(subscribers)
+        num_subscribers = number_of_subscribers(subreddit)
+        print(num_subscribers)
