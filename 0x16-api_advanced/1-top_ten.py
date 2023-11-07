f40d7418eebb22c01df559224b17e5fbd9fd4206
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-11-07 10:44:49 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,18 +2,31 @@
 """API Reddit"""
 import requests
 
-
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts
-    listed for a given subreddit"""
+    # Define the Reddit API URL for the "hot" posts in the given subreddit
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
-    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url=url, headers=headers)
-    if response.status_code == 404:
-        print(None)
+    # Set a custom user-agent to avoid being blocked by Reddit
+    headers = {"User-Agent": "MyRedditBot"}
+
+    # Send a GET request to the Reddit API
+    response = requests.get(url, headers=headers)
+
+    # Check if the subreddit is valid
+    if response.status_code != 200:
+        print("None")
         return
-    subs = response.json().get("data")
-    for post in subs.get("children"):
-        post_details = post.get("data")
-        print(post_details.get("title"))
+
+    # Parse the JSON response
+    data = response.json()
+
+    # Extract and print the titles of the first 10 hot posts
+    for i, post in enumerate(data['data']['children']):
+        print(f"{i + 1}. {post['data']['title']}")
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
