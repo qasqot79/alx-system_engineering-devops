d3c4583b7ea34418e60a5ac8e6e06dc1e68b1ee6
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-11-07 10:48:31 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,29 +2,44 @@
 """Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
+import praw
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+def recurse(subreddit, hot_list=None, after=None):
+    if hot_list is None:
+        hot_list = []
+
+    # Initialize the Reddit API client
+    reddit = praw.Reddit(client_id='YOUR_CLIENT_ID',
+                         client_secret='YOUR_CLIENT_SECRET',
+                         user_agent='YOUR_USER_AGENT')
+
+    # Check if the subreddit is valid
+    try:
+        reddit.subreddits.search_by_name(subreddit, exact=True)
+    except prawcore.NotFound:
         return None
 
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
+    # Query the API for hot articles in the subreddit
+    hot_articles = reddit.subreddit(subreddit).hot(limit=25, params={'after': after})
 
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    # Accumulate titles
+    hot_list += [article.title for article in hot_articles]
+
+    # Check if there are more pages of results
+    if hot_articles._after:
+        return recurse(subreddit, hot_list, after=hot_articles._after)
+    else:
+        return hot_list
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit_name = sys.argv[1]
+        hot_titles = recurse(subreddit_name)
+        if hot_titles is not None:
+            print(len(hot_titles))
+        else:
+            print("None")
