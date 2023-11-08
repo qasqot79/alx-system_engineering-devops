b632a895beb01a23d1250dacb958cdb08aae3ae5
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2023-11-08 14:51:05 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,24 +1,6 @@
-# Puppet manifest to fix the Apache 500 error
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-# Ensure Apache package is installed
-package { 'apache2':
-  ensure => 'installed',
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
-
-# Ensure Apache service is running and enabled
-service { 'apache2':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['apache2'],
-}
-
-# Define a custom resource to fix the issue
-# You will need to replace this with the actual fix for your issue
-exec { 'fix_apache_issue':
-  command     => '/bin/echo "Fixing Apache issue"',
-  path        => '/bin',
-  refreshonly => true,
-}
-
-# Notify the custom resource when Apache service changes
-Service['apache2'] -> Exec['fix_apache_issue']
